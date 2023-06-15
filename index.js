ce863530bbe91015e62bcd50e4b82dabe624a371
Nenoeglah <134705870+Nenoeglah@users.noreply.github.com>
2023-06-16 02:35:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(distance) {
+   if (distance <= 199) {
+      return "This one is on me!";
+   } else if (distance <= 2001) {
+      return "I will gladly take your thirty bucks.";
+   } else if (distance <= 2501) {
+      return "No can do.";
+   } else {
+        return "Wow, that journey is great!";
+   }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+   return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(etiquette){
+   switch (etiquette) {
+     case "generous":
+       return "Thank you so much.";
+     case "not as generous":
+       return "Thank you.";
+     default:
+       return "Bye.";
+   }
 }
+    
